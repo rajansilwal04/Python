18076d4724b1c3bc02359315c4091cfa2b6186a7
Rajan Silwal <rajansilwal@Rajans-MacBook-Pro.local>
2025-10-04 03:32:48 -0700

    update

--- Basic/hello.py
+++ Basic/hello.py
@@ -4,4 +4,8 @@
 # print(z)i
 
 print("Hello my naem is Rajan\nWassup")
+<<<<<<< HEAD
 print("Hello my name is Rajan silwal")
+=======
+print("Hello my naem is Rajan")
+>>>>>>> parent of d78c472 (change naem to name)
